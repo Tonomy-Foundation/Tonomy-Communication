74774893e3dd0c76c2c6974de96b1ca741204232
rebal67 <rebalree1@gmail.com>
2023-03-20 15:16:36 +0100

    feat: :art: adding authentication and error handeling

--- src/users/transform-vc/transform-vc.pipe.ts
+++ src/users/transform-vc/transform-vc.pipe.ts
@@ -1,11 +1,21 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { WsException } from '@nestjs/websockets';
 import { MessageDto, MessageRto } from '../dto/message.dto';
 
 @Injectable()
 export class TransformVcPipe implements PipeTransform {
-  transform(value: MessageRto, metadata: ArgumentMetadata) {
+  async transform(value: MessageRto, metadata: ArgumentMetadata) {
     if (metadata.type === 'body') {
-      return new MessageDto(value.message);
+      try {
+        const message = new MessageDto(value.message);
+
+        const result = await message.verify();
+
+        if (!result) throw new WsException('VC not Authorized');
+        return message;
+      } catch (e) {
+        throw new WsException(e);
+      }
     }
 
     return value;
