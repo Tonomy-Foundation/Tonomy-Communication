88cccafa74acb4cba0fab11c0421b2dcd2b79dbc
rebal67 <rebalalh@gmail.com>
2023-05-12 16:22:51 +0200

    feat: add guard for authentication

---
+++ src/users/users.guard.ts
@@ -0,0 +1,19 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UsersGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const socket = context.switchToWs().getClient();
+
+    if (socket.did) return true;
+    return false;
+  }
+}
