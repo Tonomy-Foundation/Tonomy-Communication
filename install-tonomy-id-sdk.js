837ae710f48a8fcbbd09ef86238071a876785bbc
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-15 00:16:53 +0500

    added logs

--- install-tonomy-id-sdk.js
+++ install-tonomy-id-sdk.js
@@ -1,11 +1,28 @@
-const { execSync } = require('child_process');
+const { spawnSync } = require('child_process');
 
-const currentBranch = execSync('git symbolic-ref --short HEAD', {
+const currentBranch = spawnSync('git', ['symbolic-ref', '--short', 'HEAD'], {
   encoding: 'utf8',
-}).trim();
+}).stdout.trim();
+
+console.log(currentBranch);
 
 if (currentBranch !== 'master') {
-  execSync('yarn add @tonomy/tonomy-id-sdk@development', {
-    stdio: 'inherit',
-  });
+  console.log('executed');
+  const result = spawnSync(
+    'yarn',
+    ['add', '@tonomy/tonomy-id-sdk@development'],
+    {
+      stdout: 'inherit',
+    },
+  );
+
+  console.log('package detail', result);
+
+  if (result.status !== 0 || result.error || result.signal) {
+    console.error(
+      'Failed to add package:',
+      result.error || result.signal || result.stderr,
+    );
+    process.exit(1);
+  }
 }
