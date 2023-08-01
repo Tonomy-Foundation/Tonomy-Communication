2561dcc1ef748e36fed12e2719b8d9d44e2f26f0
Jack Tanner <jack@jackandtheblockstalk.com>
2023-08-01 23:49:15 +0200

    refactor: moved API to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AsyncApiDocumentBuilder, AsyncApiModule } from 'nestjs-asyncapi';
 import { AppModule } from './app.module';
 import { setSettings } from '@tonomy/tonomy-id-sdk';
 import settings from './settings';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { setupAsyncApi, setupOpenApi } from './api';
 
 setSettings({
   blockchainUrl: settings.config.blockchainUrl,
@@ -15,36 +14,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  const config = new DocumentBuilder()
-    .setTitle('Tonomy Communication service')
-    .setDescription(
-      'Communication service connects client browser to the app and sends notification to the tonomy id app',
-    )
-    .addServer('http://localhost:5000')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('openapi', app, document, {
-    jsonDocumentUrl: '/openapi.json',
-  });
-
-  const asyncApiOptions = new AsyncApiDocumentBuilder()
-    .setTitle('Tonomy Communication service')
-    .setDescription(
-      'Communication service connects client browser to the app and sends notification to the tonomy id app',
-    )
-    .setDefaultContentType('application/json')
-    .addServer('users', {
-      protocol: 'socket.io',
-      url: 'localhost:5000',
-    })
-    .build();
-  const asyncapiDocument = await AsyncApiModule.createDocument(
-    app,
-    asyncApiOptions,
-  );
-
-  await AsyncApiModule.setup('/asyncapi', app, asyncapiDocument);
+  await setupOpenApi(app);
+  await setupAsyncApi(app);
 
   await app.listen(5000);
 }
