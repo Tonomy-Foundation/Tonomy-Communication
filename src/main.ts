839aa803eb108d88d0ce38ec349e8eaecd1ba7d9
Jack Tanner <jack@jackandtheblockstalk.com>
2023-08-01 23:04:06 +0200

    docs: setup openapi for healthcheck and ZAP scanner

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import myPackage from '../package.json';
 import { AsyncApiDocumentBuilder, AsyncApiModule } from 'nestjs-asyncapi';
 import { AppModule } from './app.module';
 import { setSettings } from '@tonomy/tonomy-id-sdk';
 import settings from './settings';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 setSettings({
   blockchainUrl: settings.config.blockchainUrl,
@@ -14,25 +16,38 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  const config = new DocumentBuilder()
+    .setTitle('Tonomy Communication service')
+    .setDescription(
+      'Communication service connects client browser to the app and sends notification to the tonomy id app',
+    )
+    .setVersion(myPackage.version)
+    .addServer('http://localhost:5000')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('openapi', app, document, {
+    jsonDocumentUrl: '/openapi.json',
+  });
+
   const asyncApiOptions = new AsyncApiDocumentBuilder()
     .setTitle('Tonomy Communication service')
     .setDescription(
       'Communication service connects client browser to the app and sends notification to the tonomy id app',
     )
-    .setVersion('0.0.1')
+    .setVersion(myPackage.version)
     .setDefaultContentType('application/json')
     .addServer('users', {
       protocol: 'socket.io',
       url: 'localhost:5000',
     })
     .build();
-
   const asyncapiDocument = await AsyncApiModule.createDocument(
     app,
     asyncApiOptions,
   );
 
-  await AsyncApiModule.setup('/api', app, asyncapiDocument);
+  await AsyncApiModule.setup('/asyncapi', app, asyncapiDocument);
 
   await app.listen(5000);
 }
