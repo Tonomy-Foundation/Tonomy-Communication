0746a895a15b387d61aaf08feb0280a0807b60de
Jack Tanner <jack@jackandtheblockstalk.com>
2023-08-21 23:00:59 +0200

    feat: Use helmet to set security headers

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 import { setSettings } from '@tonomy/tonomy-id-sdk';
 import settings from './settings';
 import { setupAsyncApi, setupOpenApi } from './api';
+import helmet from 'helmet';
 
 setSettings({
   blockchainUrl: settings.config.blockchainUrl,
@@ -12,6 +13,7 @@ setSettings({
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   await setupOpenApi(app);
