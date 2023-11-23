f17712705d314a013d114f8a0b03d60d7f764231
Jack Tanner <jack@jackandtheblockstalk.com>
2023-11-23 17:41:47 +0100

    chore: fixed lint errors

--- test/ws-client.helper.ts
+++ test/ws-client.helper.ts
@@ -5,53 +5,53 @@ import { WebsocketReturnType } from 'src/communication/communication.gateway';
 export const SOCKET_TIMEOUT = 1000;
 
 export async function connectSocket(): Promise<Socket> {
-    const socket = io('http://localhost:5000', {
-        autoConnect: false,
-        transports: ['websocket'],
+  const socket = io('http://localhost:5000', {
+    autoConnect: false,
+    transports: ['websocket'],
+  });
+
+  socket.connect();
+  let resolved = false;
+
+  await new Promise<void>((resolve, reject) => {
+    socket.on('connect', () => {
+      resolved = true;
+      resolve();
     });
 
-    socket.connect();
-    let resolved = false;
-
-    await new Promise<void>((resolve, reject) => {
-        socket.on('connect', () => {
-            resolved = true;
-            resolve();
-        });
-
-        setTimeout(() => {
-            if (resolved) return;
-            reject(new Error('Websocket connection event timed out'));
-        }, SOCKET_TIMEOUT);
-    });
-    return socket;
+    setTimeout(() => {
+      if (resolved) return;
+      reject(new Error('Websocket connection event timed out'));
+    }, SOCKET_TIMEOUT);
+  });
+  return socket;
 }
 
 export async function emitMessage(
-    socket: Socket,
-    event: string,
-    body: any,
+  socket: Socket,
+  event: string,
+  body: any,
 ): Promise<any> {
-    const res: WebsocketReturnType = await new Promise((resolve, reject) => {
-        socket
-            .timeout(SOCKET_TIMEOUT)
-            .emit(event, body, (error: any, response: any) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
+  const res: WebsocketReturnType = await new Promise((resolve, reject) => {
+    socket
+      .timeout(SOCKET_TIMEOUT)
+      .emit(event, body, (error: any, response: any) => {
+        if (error) {
+          reject(error);
+          return;
+        }
 
-                resolve(response);
-            });
-    });
+        resolve(response);
+      });
+  });
 
-    if (res.status !== HttpStatus.OK) {
-        const err = new Error(res.error);
+  if (res.status !== HttpStatus.OK) {
+    const err = new Error(res.error);
 
-        // @ts-expect-error - code is not a property of Error
-        err.code = res.status;
-        throw err;
-    } else {
-        return res.details;
-    }
+    // @ts-expect-error - code is not a property of Error
+    err.code = res.status;
+    throw err;
+  } else {
+    return res.details;
+  }
 }
