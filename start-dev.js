76f674e3cdb6a14ca9ca9f037f2f3f12e3b8ca21
Jack Tanner <jack@jackandtheblockstalk.com>
2024-07-05 15:27:36 +0200

    build:
    using nodemon to add .js extension to local imports in build

---
+++ start-dev.js
@@ -0,0 +1,37 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Path to your JS extension modification script
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const addJsExtensionScriptPath = path.join(__dirname, 'add-js-extension.js');
+
+function runCommand(command) {
+    return new Promise((resolve, reject) => {
+        const [cmd, ...args] = command.split(/\s+/); // Split the command string into command and arguments
+        const process = spawn(cmd, args, { stdio: 'inherit' }); // Use 'inherit' to display output in the parent's stdio
+
+        process.on('close', (code) => {
+            if (code === 0) {
+                resolve();
+            } else {
+                reject(new Error(`Command failed with exit code ${code}`));
+            }
+        });
+    });
+}
+
+async function startDev() {
+    try {
+        // Build the project
+        console.log('Building the project...');
+        await runCommand('yarn run build');
+        // Start the Nest application
+        console.log('Starting the application...');
+        await runCommand('yarn run start:prod');
+    } catch (error) {
+        console.error('Failed to start the application:', error);
+    }
+}
+
+startDev();
