5bfef3902b268d2213fc24aa242c526fcaf12494
Jekin Gohel <jekin1991@gmail.com>
2025-06-23 17:13:27 +0530

    Resolve build error

--- src/veriff/veriff.module.ts
+++ src/veriff/veriff.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { VeriffController } from './veriff.controller';
 import { VeriffService } from './veriff.service';
 import { HttpModule } from '@nestjs/axios';
+import {
+  VeriffWatchlistService,
+  VerifiableCredentialFactory,
+} from './veriff.helpers';
+import { CommunicationService } from '../communication/communication.service';
+import { CommunicationGateway } from '../communication/communication.gateway';
 
 @Module({
   imports: [HttpModule],
   controllers: [VeriffController],
-  providers: [VeriffService],
+  providers: [
+    VeriffService,
+    VeriffWatchlistService,
+    {
+      provide: VerifiableCredentialFactory,
+      useValue: new VerifiableCredentialFactory(),
+    },
+    Logger,
+    CommunicationService,
+    CommunicationGateway,
+  ],
 })
 export class VerffModule {}
