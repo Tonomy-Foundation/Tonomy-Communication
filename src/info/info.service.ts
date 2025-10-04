53701c7566791da3716b5c77dc11b14aadb7e6a0
Jack Tanner <jack@jackandtheblockstalk.com>
2025-10-04 22:07:44 +0200

    feat: get circulating supply

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -1,9 +1,47 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
-import { EosioTokenContract } from '@tonomy/tonomy-id-sdk';
+import {
+  assetToDecimal,
+  EosioTokenContract,
+  getStakingContract,
+  getVestingContract,
+} from '@tonomy/tonomy-id-sdk';
 import Decimal from 'decimal.js';
 
 function getTotalCoins(): string {
   return new Decimal(EosioTokenContract.TOTAL_SUPPLY).toFixed(6);
+}
+
+async function getVestedCoins(): Promise<Decimal> {
+  const uniqueHolders = await getVestingContract().getAllUniqueHolders();
+  const allAllocations = await getVestingContract().getAllAllocations(
+    uniqueHolders,
+    false,
+  );
+
+  return allAllocations.reduce<Decimal>(
+    (previous, allocation) =>
+    (previous += assetToDecimal(allocation.tokensAllocated)
+      .minus(assetToDecimal(allocation.tokensWithdrawn))
+      .toNumber()),
+    new Decimal(0),
+  );
+}
+
+async function getStakedCoins(): Promise<Decimal> {
+  const settings = await getStakingContract().getSettings();
+
+  return new Decimal(settings.totalStaked);
+}
+
+async function getCirculatingCoins(): Promise<string> {
+  const totalCoins = getTotalCoins();
+  const vestedCoins = await getVestedCoins();
+  const stakedCoins = await getStakedCoins();
+  const circulatingCoins = new Decimal(totalCoins)
+    .minus(vestedCoins)
+    .minus(stakedCoins);
+
+  return circulatingCoins.toFixed(6);
 }
 
 @Injectable()
@@ -27,7 +65,7 @@ export class InfoService {
     // Stub implementation. Replace with real CoinGecko fetch logic.
     // Intentionally not using network calls yet.
     return {
-      result: getTotalCoins(),
+      result: getCirculatingCoins(),
     };
   }
 
@@ -40,22 +78,16 @@ export class InfoService {
       );
     }
 
-    if (query.toLowerCase() === 'totalcoins') {
-      return getTotalCoins();
+    switch (query ? query.toLowerCase() : '') {
+      case 'totalcoins':
+        return getTotalCoins();
+      case 'circulating':
+        return getCirculatingCoins();
+      default:
+        throw new HttpException(
+          'Unsupported cmc query. Allowed values: totalcoins, circulating',
+          HttpStatus.BAD_REQUEST,
+        );
     }
-
-    if (!['totalcoins', 'circulating'].includes(query.toLowerCase())) {
-      throw new HttpException(
-        'Unsupported cmc query. Allowed values: totalcoins, circulating',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    return {
-      source: 'coinmarketcap',
-      status: 'stub',
-      query,
-      data: null,
-    };
   }
 }
