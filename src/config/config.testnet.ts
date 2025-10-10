64d0f8d2035c8c2c78affce749482cb93cee908f
sadiabbasi <sadiaabbas326@gmail.com>
2025-10-10 11:56:39 +0300

    fix: :zap: update testnet base address

--- src/config/config.testnet.ts
+++ src/config/config.testnet.ts
@@ -5,5 +5,5 @@ export default {
   loggerLevel: 'info',
   baseNetwork: 'base-sepolia' as const,
   baseRpcUrl: 'https://base-sepolia.infura.io/v3/',
-  baseTokenAddress: '0xC71Ae7EBa91037Ff145671D0d25Ba415BB69450c',
+  baseTokenAddress: '0x56aD9925f417358640746266eF44a701622c54Ba',
 };
