4bbba2aeb8b55619afa1c4d71b75988a051ba6d9
sadiabbasi <sadiaabbas326@gmail.com>
2025-10-20 21:40:49 +0300

    fix: :zap: updaate address and sdk

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -5,5 +5,5 @@ export default {
   loggerLevel: 'info',
   baseNetwork: 'base' as const,
   baseRpcUrl: 'https://base-mainnet.infura.io/v3/',
-  baseTokenAddress: 'TODO:',
+  baseTokenAddress: '0x36a0E9d5bb7322b26185D76Fb3fce46a40007e90',
 };
