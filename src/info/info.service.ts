3300c6412fbb693e4ce3eba5959f6fd70b4d993f
Jack Tanner <jack@jackandtheblockstalk.com>
2025-11-11 09:47:37 +0100

    refactor: moved getAllActions()

--- src/info/info.service.ts
+++ src/info/info.service.ts
@@ -12,6 +12,7 @@ import {
 } from '@tonomy/tonomy-id-sdk';
 import Decimal from 'decimal.js';
 import { getPriceCoinGecko } from './price';
+import { getAllActions } from './actions';
 
 const CACHE_DURATION_MS = 24 * 60 * 60 * 1000; // 1 day
 
@@ -223,130 +224,23 @@ async function getNetwork(): Promise<NetworkInfo> {
   };
 }
 
-function getHost(): string {
-  switch (getSettings().environment) {
-    case 'production':
-      return 'pangea.eosusa.io';
-    case 'testnet':
-      return 'test.pangea.eosusa.io';
-    default:
-      throw new Error(
-        `environment ${getSettings().environment} not supported for fetching all vesting holders`,
-      );
-  }
-}
-
-// https://hyperion.docs.eosrio.io/4.0/api/v2/#v2historyget_actions
-type ActionQuery = {
-  account?: string;
-  filter?: string;
-  track?: string;
-  skip?: number;
-  limit?: number;
-  sort?: string;
-  block_num?: string;
-  global_sequence?: string;
-  after?: string;
-  before?: string;
-  simple?: boolean;
-  noBinary?: boolean;
-  checkLib?: boolean;
-};
-
-type ActionResponse = {
-  '@timestamp': string;
-  timestamp: string;
-  block_num: number;
-  block_id: string;
-  trx_id: string;
-  act: {
-    account: string;
-    name: string;
-    authorization: { actor: string; permission: string }[];
-    data: any;
-  };
-  receipts: Array<{
-    receiver: string;
-    global_sequence: number;
-    recv_sequence: number;
-    auth_sequence: Array<{ account: string; sequence: number }>;
-  }>;
-  cpu_usage_us?: number;
-  net_usage_words?: number;
-  account_ram_deltas?: Array<{ account: string; delta: number }>;
-  global_sequence: number;
-  producer: string;
-  action_ordinal: number;
-  creator_action_ordinal: number;
-  signatures?: string[];
-};
-
-async function getActions(
-  query?: ActionQuery,
-): Promise<ActionResponse[] | undefined> {
-  let url = `https://${getHost()}/v2/history/get_actions`;
-
-  if (query) {
-    const params = new URLSearchParams();
-
-    for (const key of Object.keys(query)) {
-      const value = (query as any)[key];
-
-      if (value !== undefined) {
-        params.append(key, value.toString());
-      }
-    }
-
-    url += `?${params.toString()}`;
-  }
-
-  const res = await fetch(url);
-  const data = await res.json();
-
-  return data.actions;
-}
-
-async function getAllActions(query?: ActionQuery): Promise<ActionResponse[]> {
-  let allActions: ActionResponse[] = [];
-  let skip = 0;
-  const limit = 1000;
-
-  console.log('Fetching actions with query', query);
-
-  while (true) {
-    console.log(`Fetching actions from ${skip} to ${skip + limit}...`);
-    const actions = await getActions({
-      ...query,
-      skip,
-      limit,
-      noBinary: true,
-    });
-
-    if (!actions || actions.length < limit) {
-      break;
-    }
-
-    allActions = allActions.concat(actions);
-
-    if (actions.length > 0) {
-      console.log(
-        `Fetched ${actions.length} actions from ${actions[0].timestamp} to ${actions[actions.length - 1].timestamp}`,
-      );
-    }
-
-    skip += limit;
-  }
-
-  return allActions;
-}
-
 async function getTransactionInfo(): Promise<TransactionInfo> {
+  if (
+    getSettings().environment !== 'production' &&
+    getSettings().environment !== 'testnet'
+  ) {
+    return {
+      total: 0,
+      transfers: 0,
+    };
+  }
+
   const now = new Date();
   const oneDayAgo = new Date(now.getTime() - 24 * 60 * 60 * 1000);
   // const oneDayAgo = new Date('2024-01-01T00:00:00.000Z'); // (fetch ALL transactions since start of chain)
   const transactions = await getAllActions({
     after: oneDayAgo.toISOString(),
-    // before: now.toISOString(),
+    before: now.toISOString(),
   });
   const transfers = transactions.filter(
     (action) => action.act.name === 'transfer',
