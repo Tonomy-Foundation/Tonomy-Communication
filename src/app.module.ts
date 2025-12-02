dbf2d0f8e8b9db657e3b6b9a5f7333b12e061b11
Jack Tanner <jack@jackandtheblockstalk.com>
2025-12-02 13:15:28 +0100

    feat: cron job setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommunicationModule } from './communication/communication.module';
 import { AccountsModule } from './accounts/accounts.module';
 import { VerffModule } from './veriff/veriff.module';
 import { InfoModule } from './info/info.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [CommunicationModule, AccountsModule, VerffModule, InfoModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    CommunicationModule,
+    AccountsModule,
+    VerffModule,
+    InfoModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
