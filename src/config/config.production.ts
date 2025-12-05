056c0b4333f0b7ebf24e3aa1e59f26fa9b719bd9
Jack Tanner <jack@jackandtheblockstalk.com>
2025-12-05 18:45:40 +0100

    feat: baseMintBurnAddress

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -6,5 +6,5 @@ export default {
   baseNetwork: 'base' as const,
   baseRpcUrl: 'https://base-mainnet.infura.io/v3/',
   baseTokenAddress: '0x36a0E9d5bb7322b26185D76Fb3fce46a40007e90',
-  baseMintBurnAddress: '0xAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAaAa',
+  baseMintBurnAddress: '0x86d1Df3473651265AA88E48dE9B420debCa6e676',
 };
