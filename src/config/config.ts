6a141b62943363a12a79ee18cdd0ddf01aed482c
sadiabbasi <sadiaabbas326@gmail.com>
2025-12-10 20:36:12 +0300

    fix: :zap: fixed config

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,4 +7,5 @@ export default {
   basePrivateKey:
     '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e', // Hardhat account #19
   baseMintBurnAddress: '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199',
+  baseTokenAddress: '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199',
 };
